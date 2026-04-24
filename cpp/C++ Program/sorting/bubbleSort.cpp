5882fb880bcdbb2cb526bb9434ab660797498ea8
Jibon Raihan <mdraihaanjourdar@gmail.com>
2026-04-25 00:25:58 +0600

    added search feature

---
+++ cpp/C++ Program/sorting/bubbleSort.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    bool isSwap;
+    for (int i = 0; i < n - 1; i++)
+    {
+        isSwap = false;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap) break;
+    }
+    if (!isSwap)
+    {
+        cout << "No sort needed." << endl;
+    }
+    else
+    {
+        cout << "Sort done!" << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter number of element: ";
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+    cout << "Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
